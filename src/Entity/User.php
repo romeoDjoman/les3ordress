edfa5d7f3473f7fc1510e409cc395fda91d20629
Maye <128099711+Soukhra@users.noreply.github.com>
2023-08-27 13:01:06 +0200

    Ajout des liens de compte

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -159,4 +159,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+    public function __toString()
+    {
+        return $this->prenom;
+    }
 }
